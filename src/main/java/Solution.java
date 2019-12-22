9e54c6b648e1e30d1fc894871a2e8f3e83aa97cd
Denys Klymenko <suncoin@gmail.com>
2019-12-22 19:32:21 +0200

    Initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the whatFlavors function below.
+    //https://web.stanford.edu/class/cs9/sample_probs/TwoSum.pdf
+
+    static void whatFlavors(int[] cost, int money) {
+        Map<Integer, Integer> costMap = new HashMap<>();
+
+        for (int i = 0; i < cost.length - 1; i++) {
+                if (costMap.containsKey((money - cost[i]))) {
+                    System.out.println(costMap.get(money - cost[i]) + " " + String.valueOf(i + 1));
+                    return;
+                }
+                costMap.put(cost[i], i+1);
+            }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
